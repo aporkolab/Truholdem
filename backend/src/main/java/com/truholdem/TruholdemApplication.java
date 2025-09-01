b9065d1d06cebeb53c36311f66fd5bbfeedfe32e
APorkolab <adam.porkolab@gmail.com>
2025-09-01 15:45:00 +0200

    feat: add spring boot main application class

---
+++ backend/src/main/java/com/truholdem/TruholdemApplication.java
@@ -0,0 +1,14 @@
+package com.truholdem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+public class TruholdemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruholdemApplication.class, args);
+    }
+
+}
